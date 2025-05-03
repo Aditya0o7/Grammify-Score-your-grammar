be6c6ccf5e67032c3cf7251e7c44de5e44a0d99d
Aditya Sharma <adityasharmao371@gmail.com>
2025-05-03 23:41:10 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000';
 const instance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: backendUrl,
 });
 
 export default instance;
